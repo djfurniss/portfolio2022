28ef1765ac99163487e8dab845e4d0687bada965
Denasia <dfurniss@ymail.com>
2022-11-11 13:49:36 -0500

    a file full of the descriptions for each project, imported into Projects so it's not full of long text

---
+++ src/projects/descriptions.js
@@ -0,0 +1,11 @@
+module.exports = {
+    restaurantReservation: "This web app is designed for restaurant staff to organize their reservations. ",
+
+    grubDash: "This project helps restaurants keep track of their customer's orders. Implementing a CRUD design, customers can add, delete, or update orders. Verification middleware makes sure customers cannot edit an order that has already been delivered or is in the process of being deliered.",
+
+    library: "I built a functioning server for a local library's front end application to make API calls. I implemented a RESTful API.",
+
+    decoderRing: "This application features three unique methods of encrypting and decrypting messages. I was responsible for implementing functional JavaScript operations using efficient, readable, and DRY code.",
+
+    grouPlay: "This app is my favorite thing that I'm working on currently! It's designed to be a place where a group of friends can control their music together. When with friends, there's usually a descussion or argument about who is going to be responsible for playing music. Having a shared playlist and music player in one eliminates that problem."
+};
