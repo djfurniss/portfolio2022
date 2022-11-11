73660a76acbe92d8697f7209e35febec8caf4cc0
Denasia <dfurniss@ymail.com>
2022-11-11 14:07:03 -0500

    installed icon library to add home icon to HomeButton

--- src/common/HomeButton.js
+++ src/common/HomeButton.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { AiOutlineHome } from 'react-icons/ai'
 
 // TODO: use an icon
 
@@ -10,12 +11,18 @@ export default function HomeButtom(){
         <div 
             onClick={()=>navigate("/")}
             style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
                 position: 'absolute',
                 top: 30,
                 left: 30,
                 width: '30px',
                 height: '30px',
-                backgroundColor: 'black',
-            }}></div>
+                border: '1px black solid',
+                borderRadius: '1vw'
+            }}>
+                <AiOutlineHome style={{color: 'black'}}/>
+            </div>
     )
 }
