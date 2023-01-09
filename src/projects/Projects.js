fc01938c73173680b5557e6d94961b5a12d4d3dd
Denasia <dfurniss@ymail.com>
2023-01-08 23:21:05 -0600

    added calculator and payback projects

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -11,21 +11,21 @@ export default function Projects(){
             <HomeButtom/>
             <div className="scroll" id="main-container">
                 <Project 
-                    name="Restaurant Reservation" 
-                    stack="React, Express, Node.js, PostgreSQL"
-                    description={desc.restaurantReservation}
-                    repo="https://github.com/djfurniss/decoder-ring"
-                    link="https://restaurant-res-client.onrender.com/dashboard"/>
-                <Project 
-                    name="Library Records Application" 
-                    description={desc.library}
-                    repo="https://github.com/djfurniss/local-library-project"
-                    link="https://local-library-seven.vercel.app/"/>
+                    name="PayBack"
+                    stack="JavaScript, React, CSS, MangoDB" 
+                    description={desc.payBack}
+                    repo="https://github.com/djfurniss/BORROW-APP"/>
                 <Project 
                     name="grouPlay" 
                     stack="React Native, Node.js, Express, PostgreSQL"
                     description={desc.grouPlay}
                     repo="https://github.com/djfurniss/grouPlayApp"/>
+                <Project 
+                    name="Restaurant Reservation" 
+                    stack="React, Express, Node.js, PostgreSQL"
+                    description={desc.restaurantReservation}
+                    repo="https://github.com/djfurniss/decoder-ring"
+                    link="https://restaurant-res-client.onrender.com/dashboard"/>
                 <Project 
                     name="Secret Message Encryption and Decryption" 
                     description={desc.decoderRing}
@@ -41,6 +41,17 @@ export default function Projects(){
                     stack="React, Bootstrap" 
                     description={desc.flashcardOMatic}
                     repo="https://github.com/djfurniss/flashcard-o-matic"/>
+                <Project 
+                    name="Calculator"
+                    stack="HTML, CSS, JavaScript" 
+                    description={desc.calc}
+                    repo="https://github.com/djfurniss/calculator"
+                    link="https://calculator-s0kj.onrender.com"/>
+                <Project 
+                    name="Library Records Application" 
+                    description={desc.library}
+                    repo="https://github.com/djfurniss/local-library-project"
+                    link="https://local-library-seven.vercel.app/"/>
             </div>
         </div>
     )
--- src/projects/descriptions.js
+++ src/projects/descriptions.js
@@ -9,5 +9,9 @@ module.exports = {
 
     grouPlay: "This app is my favorite thing that I'm working on currently! It's designed to be a place where a group of friends can control their music together. When with friends, there's usually a descussion or argument about who is going to be responsible for playing music. Having a shared playlist and music player in one eliminates that problem.",
 
-    flashcardOMatic : "Flashcard-o-matic is designed to help students organize their study material using flashcards. With this app, you can create decks to keep topics and subjects in one place. You can then add cards to decks and study them. With a simple and easy to use UI, Flashcard-o-matic makes it easy to study with no distractions. This project was part of an assignment in Thinkful's software engineering immersion bootcamp. This project focused heavilly on my understanding of React including hooks like useState and useEffect."
+    flashcardOMatic : "Flashcard-o-matic is designed to help students organize their study material using flashcards. With this app, you can create decks to keep topics and subjects in one place. You can then add cards to decks and study them. With a simple and easy to use UI, Flashcard-o-matic makes it easy to study with no distractions. This project was part of an assignment in Thinkful's software engineering immersion bootcamp. This project focused heavilly on my understanding of React including hooks like useState and useEffect.",
+
+    calc: "Yes, it's a calculator! With HTML, DOM manipulation, and some modern styling this simple but handy project came to life. I made sure to develop functionality that typical calculators would have. You can trail and chain equations such as 3 + 5 - 8. After clicking =, you can continue an equation. There are two buttons, clear and backspace to allow for editing on the calculator's screen.",
+
+    payBack: "Girl Develop It Hackathon 2022 award winning app for Most Creative. I had the privilege of working with seven extremely talented ladies during this event to develop an app where users can keep track of borrowed money as the lender or borrower."
 };
