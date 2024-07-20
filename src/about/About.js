6a2a6f3e38595567b977f1934500aac0d6f708ec
Denasia <dfurniss@ymail.com>
2024-07-20 18:22:28 -0500

    updated about information, mainly in the journey into tech section

--- src/about/About.js
+++ src/about/About.js
@@ -22,7 +22,7 @@ export default function About(){
                 >
                     <h2>a little about me</h2>
                     <div>
-                        <p>Hello there! I am 23 year old Denasia. I was born and mostly raised in Mississippi. I've moved around a lot but now I am back in my home town. I adore everything Star Wars and Japan related. I love to rollerskate and spend time with family and friends. I absolutely love music. I was a band kid in high school, I played the clarinet, taught myself to play the flute and dabbled in other instruments. I ended up getting into color guard and did that during marching season and went back to my clarinet roots for concert season. I love languages. At the moment I'm learning Japanese and Spanish. Needless to say, I love learning new things, developing skills, and putting my brain to work!
+                        <p>Hello there! I am 24 year old Denasia. I was born and mostly raised in Mississippi. I've moved around a lot but now I am back in my home town. I adore everything Star Wars and Japan related. I love to rollerskate, crochet, and spend time with family and friends. I absolutely love music. I was a band kid in middle and high school, I played the clarinet, taught myself to play the flute and dabbled in other instruments. I ended up getting into color guard and did that during marching season and went back to my clarinet roots for concert season. I love languages. At the moment I'm learning Japanese and Spanish. Needless to say, I love learning new things, developing skills, and putting my brain to work!
                         </p>
                     </div>
                 </motion.div>
@@ -34,7 +34,7 @@ export default function About(){
                 >
                 <h2>my journey into tech</h2>
                 <div>
-                    <p>Making the career switch from ASL interpreter to software engineer has been the best decision I've made in a long time! I've always had a knack for tech but since I already known sign language since my sophmore year of high school, I made the simple decision to make it my career. I loved it and I still use sign language to this day. What I quickly learned, though, is that from a career standpoint, I wanted to be involved in a profession that challenged me on a regular basis, where I can wear many hats and participate in something bigger than myself. I learned a few things about myself too along the way. As a math lover, it's not a surprise that I'm always up for a challenge. My brain loves to be picked at. And so, here I am. - Now a software engineer. I am super excited to build, connect, and grow in this field.
+                    <p>I've always been a pretty tech-savy person so it wasn't surprising that I wanted to learn how to code. I spent my high school career involved in robotics and STEM classes. I only started taking software development seriously when I chose to quit my job as an ASL interpreter. I decided to enroll in a fulltime 6 month bootcamp in 2022 to learn as much as I could as fast as I could so I could start my career switching journey. As it turned out, the job search didn't go as planned, time moved on, and I never did land a job as a software developer. That, however, doesn't mean I'm throwing away the things I have learned along the way! I still have such a strong love for coding and still plan to use the skills I carry. I currently work in the banking industry but my tech roots are sure to drive me right smack in the middle of the beloved FinTech!
                     </p>
                 </div>
                 </motion.div>
@@ -46,7 +46,7 @@ export default function About(){
                 >
                 <h2>education</h2>
                 <div>
-                    <p>I obtained my associates degree from Mississippi Gulf Coast Community College majoring in interpreting training technology, of course. I've also completed Thinkful's Software Engineering immersion course. I plan on getting my Bachelor's in computer science from WGU. I'm determined to also gain more certifications in the near future.
+                    <p>I obtained my associates degree from Mississippi Gulf Coast Community College majoring in interpreting training technology, leading me to just a year long career of being an ASL interpreter. I've also completed Thinkful's Software Engineering immersion course. I'm determined to gain more certifications in the near future.
                     </p>
                 </div> 
                 </motion.div>
